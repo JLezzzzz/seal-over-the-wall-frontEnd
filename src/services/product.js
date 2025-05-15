1e7e3ac5765c25c369752b042afed51d39dacf92
Khomgrit_Ton <khomgrit.ph@gmail.com>
2025-05-16 02:48:54 +0700

    updated .env

--- src/services/product.js
+++ src/services/product.js
@@ -14,7 +14,7 @@ export const getProductById = async (productId) => {
 
 // Product upload
 const uploadProduct = async (data) => {
-  console.log("📦 Uploading product:", data); // <- debug line
+  // console.log("📦 Uploading product:", data); // <- debug line
   const response = await api.post("/product", data);
   return response.data;
 };
